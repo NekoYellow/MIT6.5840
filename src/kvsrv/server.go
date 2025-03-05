5698ce2ca55480f0d14b8ad3c7816061678a8e75
Yueyuan Huang <harvyhyy@gmail.com>
2025-03-06 01:02:35 +0800

    Implement Key/value server with no network failures

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,30 +14,36 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
 	mu sync.Mutex
-
-	// Your definitions here.
+	mp map[string]string
 }
 
-
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	reply.Value = kv.mp[args.Key]
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	kv.mp[args.Key] = args.Value
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	oldValue := kv.mp[args.Key]
+	kv.mp[args.Key] = oldValue + args.Value
+	reply.Value = oldValue
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
 
-	// You may need initialization code here.
+	kv.mu = sync.Mutex{}
+	kv.mp = map[string]string{}
 
 	return kv
 }
